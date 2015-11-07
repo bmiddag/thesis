35229d97fe786a1f1eb3cc56692cd9d007ddcd81
Bart Middag <bart.middag@ugent.be>
2015-11-07 21:39:00 +0100

    Begin work on graph grammar demo

---
+++ Thesis/Assets/Scripts/Demo/Node.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Demo {
+	public class Node : MonoBehaviour {
+		Grammars.Graph.Node node;
+		SpriteRenderer spriteRender;
+
+		// Use this for initialization
+		void Start() {
+			spriteRender = gameObject.AddComponent<SpriteRenderer>();
+			UpdateSprite();
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+
+		// Custom function with draw code
+		void UpdateSprite() {
+			if (node != null) {
+				if (node.hasAttribute("demo__shape")) {
+					string shape = node.getAttribute("demo__shape");
+					string upperShape = char.ToUpper(shape[0]) + shape.Substring(1);
+					print(upperShape);
+					spriteRender.sprite = Resources.Load<Sprite>("Sprites/" + upperShape);
+				}
+				if (node.hasAttribute("demo__color")) {
+					string color = node.getAttribute("demo__color");
+					switch (node.getAttribute("demo__color")) {
+						case "red":
+							spriteRender.color = Color.red;
+							break;
+						case "green":
+							spriteRender.color = Color.green;
+							break;
+						case "blue":
+							spriteRender.color = Color.blue;
+							break;
+						case "yellow":
+							spriteRender.color = Color.yellow;
+							break;
+						default:
+							spriteRender.color = Color.white;
+							break;
+					}
+				}
+			}
+		}
+
+		public void setNode(Grammars.Graph.Node node) {
+			this.node = node;
+		}
+
+		public Grammars.Graph.Node getNode() {
+			return node;
+		}
+
+		public string getAttribute(string key) {
+			return node.getAttribute(key);
+		}
+
+		public void setAttribute(string key, string value) {
+			node.setAttribute(key, value);
+			if (key.Contains("demo__")) UpdateSprite();
+		}
+    }
+}
---
+++ Thesis/Assets/Scripts/Grammars/Graph/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grammars.Graph {
+	public class Node {
+		Dictionary<string, string> attributes = new Dictionary<string, string>();
+		string name;
+
+		public Node(string name) {
+			this.name = name;
+		}
+
+		public bool hasAttribute(string key) {
+			return attributes.ContainsKey(key);
+		}
+
+		public string getAttribute(string key) {
+			return attributes[key];
+		}
+
+		public void setAttribute(string key, string value) {
+			attributes[key] = value;
+		}
+	}
+}
