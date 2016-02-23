de9ef7bd6993caac7df5df628b8fc614ebe9df9a
Bart Middag <bart.middag@ugent.be>
2016-02-24 00:31:03 +0100

    Shortcuts (tab/enter) for fast modification of attributes (no more clicking involved)

--- Thesis/Assets/Scripts/Demo/GraphDemoController.cs
+++ Thesis/Assets/Scripts/Demo/GraphDemoController.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 using Grammars.Graph;
 using Grammars;
 using System;
+using UnityEngine.EventSystems;
 
 namespace Demo {
     public class GraphDemoController : MonoBehaviour {
+        EventSystem system;
+
         public GraphRenderer currentGraphRenderer;
         public bool paused = false;
 
@@ -18,13 +21,31 @@ namespace Demo {
         
         // Use this for initialization
         void Start() {
-            
+            system = EventSystem.current;
         }
 
         // Update is called once per frame
         void Update() {
             if (Input.GetKeyDown(KeyCode.A) && currentGraphRenderer.currentNode != null && !paused) {
                 OpenAttributePopUp();
+            }
+            if (paused && attributePopUp.activeSelf) {
+                if (Input.GetKeyDown(KeyCode.Tab)) {
+                    Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+
+                    if (next != null) {
+                        InputField inputfield = next.GetComponent<InputField>();
+                        if (inputfield != null)
+                            inputfield.OnPointerClick(new PointerEventData(system));  //if it's an input field, also set the text caret
+                        system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.Return)) {
+                    CloseAttributePopUp();
+                }
+                if (Input.GetKeyDown(KeyCode.Escape)) {
+                    CancelAttributePopUp();
+                }
             }
         }
 
@@ -35,6 +56,7 @@ namespace Demo {
             if (currentNode != null) {
                 paused = true;
                 attributePopUp.SetActive(true);
+                attributeNameField.OnPointerClick(new PointerEventData(system));
             }
         }
 
@@ -44,6 +66,13 @@ namespace Demo {
             if (name != "" && value != "") {
                 currentNode.SetAttribute(name, value);
             }
+            paused = false;
+            attributePopUp.SetActive(false);
+        }
+
+        public void CancelAttributePopUp() {
+            attributeNameField.text = "";
+            attributeValueField.text = "";
             paused = false;
             attributePopUp.SetActive(false);
         }
